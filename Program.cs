5989dbb782ee9496f853743a79ef6c3d735204f3
Kaylany Paulus <kaylanysilvapaulus@gmail.com>
2021-03-17 18:58:22 -0300

    Sonic seu perfeito, mais para frente vou te pintar S2.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("                      ...,?77??!~~~~!???77?<~.... ");
+            Console.WriteLine("                 ..?7`                           `7!..");
+            Console.WriteLine("             .,=`          ..~7^`   I                  ?1.");
+            Console.WriteLine("........  ..^            ?`  ..?7!1 .               ...??7");
+            Console.WriteLine(".        .7`        .,777.. .I.    . .!          .,7!");
+            Console.WriteLine("..     .?         .^      .l   ?i. . .`       .,^");
+            Console.WriteLine(" b    .!        .= .?7???7~.     .>r .      .=");
+            Console.WriteLine(" .,.?4         , .^         1        `     4...");
+            Console.WriteLine("  J   ^         ,            5       `         ?<");
+            Console.WriteLine(" .%.7;         .`     .,     .;                   .=.");
+            Console.WriteLine(" .+^ .,       .%      MML     F       .,             ?,");
+            Console.WriteLine("  P   ,,      J      .MMN     F        6               4.");
+            Console.WriteLine("  l    d,    ,       .MMM!   .t        ..               ,,");
+            Console.WriteLine("  ,    JMa..`         MMM`   .         .!                .;");
+            Console.WriteLine("   r   .M#            .M#   .%  .      .~                 .,");
+            Console.WriteLine(" dMMMNJ..!                 .P7!  .>    .         .         ,,");
+            Console.WriteLine(" .WMMMMMm  ?^..       ..,?! ..    ..   ,  Z7`        `?^..  ,,");
+            Console.WriteLine("    ?THB3       ?77?!        .Yr  .   .!   ?,");
+            Console.WriteLine("      ?,                   .,^.` .%  .^      5.");
+            Console.WriteLine("        7,          .....?7     .^  ,`        ?.");
+            Console.WriteLine("          `<.                 .= .`'           1");
+            Console.WriteLine("          ....dn... ... ...,7..J=!7,           .,");
+            Console.WriteLine("       ..=     G.,7  ..,o..  .?    J.           F");
+            Console.WriteLine("     .J.  .^ ,,,t  ,^        ?^.  .^  `?~.      F");
+            Console.WriteLine("    r %J. $    5r J             ,r.1      .=.  .%");
+            Console.WriteLine("    r .77=?4.    ``,     l ., 1  .. <.       4.,");
+            Console.WriteLine("    .$..    .X..   .n..  ., J. r .`  J.       `'");
+            Console.WriteLine("  .?`  .5        `` .%   .% .' L.'    t");
+            Console.WriteLine("  ,. ..1JL          .,   J .$.?`      .");
+            Console.WriteLine("          1.          .=` ` .J7??7<.. .;");
+            Console.WriteLine("           JS..    ..^      L        7.:");
+            Console.WriteLine("             `> ..       J.  4.");
+            Console.WriteLine("              +   r `t   r ~=..G.");
+            Console.WriteLine("              =   $  ,.  J");
+            Console.WriteLine("              2   r   t  .;");
+            Console.WriteLine("        .,7!  r   t`7~..  j..");
+            Console.WriteLine("        j   7~L...$=.?7r   r ;?1.");
+            Console.WriteLine("         8.      .=    j ..,^   ..");
+            Console.WriteLine("        r        G              .");
+            Console.WriteLine("      .,7,        j,           .>=.");
+            Console.WriteLine("   .J??,  `T....... %             ..");
+            Console.WriteLine("..^     <.  ~.    ,.             .D");
+          Console.WriteLine(".?`        1   L     .7.........?Ti..l");
+         Console.WriteLine(",`           L  .    .%    .`!       `j,");
+        Console.WriteLine(".^             .  ..   .`   .^  .?7!?7+. 1");
+       Console.WriteLine(".`              .  .`..`7.  .^  ,`      .i.;");
+       Console.WriteLine(".7<..........~<<3?7!`    4. r  `          G%");
+        Console.WriteLine("                          J.` .!           %");
+        Console.WriteLine("                            JiJ           .`");
+        Console.WriteLine("                              .1.         J");
+        Console.WriteLine("                                 ?1.     .");
+        Console.WriteLine("                                     7<..%");
+        Console.WriteLine();
+        Console.ReadLine();
+        Console.Clear();
+        }
+    }
+}
